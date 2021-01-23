01eeac510403acb39d362cf28fecde110a5bcd80
Kim <68557743+fineprintkim@users.noreply.github.com>
2021-01-23 10:25:22 -0500

    done

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -22,8 +22,8 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("data.csv").then(function(chartData) {
-
+d3.csv("./assets/data/data.csv").then(function(chartData) {
+    console.log(chartData);
     // Step 1: Parse Data/Cast as numbers
     // ==============================
     chartData.forEach(function(data) {
@@ -34,11 +34,11 @@ d3.csv("data.csv").then(function(chartData) {
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(chartData, d => d.poverty)])
+      .domain([d3.min(chartData, d => d.poverty)-1, d3.max(chartData, d => d.poverty)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(chartData, d => d.healthcare)])
+      .domain([d3.min(chartData, d => d.healthcare)-1, d3.max(chartData, d => d.healthcare)])
       .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -64,9 +64,38 @@ d3.csv("data.csv").then(function(chartData) {
     .attr("cx", d => xLinearScale(d.poverty))
     .attr("cy", d => yLinearScale(d.healthcare))
     .attr("r", "15")
-    .attr("fill", "pink")
+    .attr("fill", "green")
     .attr("opacity", ".5");
 
+  //  chartGroup.selectAll("text").data(chartData).enter().append("text")
+   // .attr("dx", d => xLinearScale(d.poverty))
+   // .attr("dy", d => yLinearScale(d.healthcare))
+  //  .attr("text-anchor","middle")
+   // .attr("font-size","12px")
+    //.text(d=>d.abbr);
+    
+    var statetxtGroup = chartGroup.selectAll(".stateText")
+    .data(chartData)
+    .enter()
+    .append("text")
+    .classed("stateText",true)
+    .attr("x", d => xLinearScale(d.poverty))
+    .attr("y", d => yLinearScale(d.healthcare))
+    .text(function(d) {
+        return d.abbr;
+    })
+    .style("text-anchor", "middle")
+    .style("font-weight", "bold")
+    .style("font-size", "7pt")
+    .style("fill", "#344761");
+    
+    function renderText(textNew, newXScale, chosenXAxis, newYScale, chosenYAxis) {
+        textNew.transition()
+            .duration(2000)
+            .attr("x", d => newXScale(d[chosenXAxis]))
+            .attr("y", d => newYScale(d[chosenYAxis]));
+        return textNew;
+    }
     // Step 6: Initialize tool tip
     // ==============================
     var toolTip = d3.tip()
@@ -97,12 +126,12 @@ d3.csv("data.csv").then(function(chartData) {
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("In Poverty (%)");
+      .text("Lacks Healthcare (%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Lack Healthcare (%)");
+      .text("In Poverty (%)");
   }).catch(function(error) {
     console.log(error);
   });
